b69e293318158a24797b45fde914480148ebca19
TheShadowRaptor <90637945+TheShadowRaptor@users.noreply.github.com>
2021-11-17 14:09:54 -0400

    Changed Split Char to %

--- TheFay/Program.cs
+++ TheFay/Program.cs
@@ -9,7 +9,7 @@ namespace TheFay
     class Program
     {
         static string[] story = new string[10];
-        static char[] splits = { ',' };
+        static char[] splits = { '%' };
         static void Main(string[] args)
         {
             Page(1);
@@ -21,7 +21,7 @@ namespace TheFay
         {
             //--------------------Arrays-----------------------------
             story[0] = ("The Temple of Mictlantecuhtli,");
-            story[1] = ("You take a deep breath of fresh forest air.,Sup opp uoop");                      
+            story[1] = ("You take a deep breath of fresh forest air.%The trees sway back and forth from the wind, yet the sky sat obscured by the meny leaves above.%More jungle lays before you, sounds of insects and animals were heard within its darkness.%%You are an archaeologist.%%You have heard rumors about a so called temple deep in this jungle.%It is said to be home of the Aztec god of death... Mictlantecuhtli.%The first of meny choices lay before you.%%Will you;%%A - Adventure Onward, or B - Leave The Forest");                      
             story[2] = ("Page2");
             story[3] = ("Page3");
             story[4] = ("Page4");
