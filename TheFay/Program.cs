90ed34a4dba9b6b1df6919201dede663475009c2
TheShadowRaptor <90637945+TheShadowRaptor@users.noreply.github.com>
2021-11-22 17:55:16 -0400

    Working on Parse

--- TheFay/Program.cs
+++ TheFay/Program.cs
@@ -8,17 +8,24 @@ namespace TheFay
 {
     class Program
     {
-        static bool isGameActive = true;
+        // intitlizes input int
+        static int choice = 0;
 
-        static bool isGameOver = true;
+        //Main program loop
+        static bool gameLoop = true;
+
+        //checks if game is active
+        static bool isStoryActive = false;
 
         static bool chose = true;
 
         static string[] story = new string[10];
 
-        static char[] splits = { '%' };
+        static char[] splits = {'%'};
 
         static char[] AB = new char[2];
+
+        static string[] page = new string[10];
 
         static void Main(string[] args)
         {
@@ -37,19 +44,30 @@ namespace TheFay
             // two destination page numbers
             // pages that end the story will be blank
 
+            // 
+            // % Splits string lines
+            // ex:
+            //
+            // Before %
+            // "I went to the store. I got milk."
+            //
+            // After %
+            // "I went to the store."
+            // "I got some milk."
+            //
             //==========================Pages======================
             //--------------------------Title----------------------
-            story[0] = ("The Temple of Mictlantecuhtli,");
+            story[0] = ("The Temple of Mictlantecuhtli.%%Press any button to begin.%%Press esc to exit");
             //-----------------------------------------------------
             //--------------------------choice 1-------------------
-            story[1] = ("You take a deep breath of fresh forest air.%The trees sway back and forth from the wind, yet the sky sat obscured by the meny leaves above.%More jungle lays before you, sounds of insects and animals were heard within its darkness.%%You are an archaeologist.%%You have heard rumors about a so called temple deep in this jungle.%It is said to be home of the Aztec god of death... Mictlantecuhtli.%The first of meny choices lay before you.%%Will you;%%A - Adventure Onward, or B - Leave The Forest?");                       
+            story[1] = ("You take a deep breath of fresh forest air.%The trees sway back and forth from the wind, yet the sky sat obscured by the meny leaves above.%More jungle lays before you, sounds of insects and animals were heard within its darkness.%%You are an archaeologist.%%You have heard rumors about a so called temple deep in this jungle.%It is said to be home of the Aztec god of death... Mictlantecuhtli.%The first of meny choices lay before you.%%Will you;%%A - Adventure Onward, or B - Leave The Forest?%%3%%4");                       
             story[2] = ("You Push onwards through the vast jungle."); // A
             story[3] = ("You descide to turn back and leave this adventure to someone more brave."); // B
             //-----------------------------------------------------
             //--------------------------choice 2-------------------
-            story[4] = ("Page4");
-            story[5] = ("Page5"); // A
-            story[6] = ("Page6"); // B
+            story[4] = ("Page4;Select A;Select B;5;6");
+            story[5] = ("Page5;Select A;Select B;7;8"); // A
+            story[6] = ("Page6;Select A;Select B;9;10"); // B
             //-----------------------------------------------------
             //--------------------------choice 3-------------------
             story[7] = ("Page7");
@@ -71,45 +89,19 @@ namespace TheFay
 
         static void GameLoop()
         {
-            while (isGameActive)
+            //In
+            while (gameLoop)
             {
-                // intitlizes input int
-                int choice = 0;
-
-                //=================================================Title=============================================================
-                Page(choice); // Title
-
-                chose = true;
-                do
-                {
-                    ConsoleKeyInfo Input = Console.ReadKey(true);
-
-                    if (Input.Key == ConsoleKey.A)
-                    {
-                        // Changes page
-                        choice = choice + 1;
-                        chose = false;
-                        isGameOver = false;
-                    }
-
-                    else if (Input.Key == ConsoleKey.B)
-                    {
-                        // Changes page
-                        isGameActive = false;
-                        break;
-                    }
-                }
-                while (chose == true);
-                //===================================================================================================================
-
+                // Displays title
+                Title();
 
                 // ===================================== Game plays well loop is active =============================================
-                while (isGameOver == false)
+                while (isStoryActive == true)
                 {
                     // Shows story
                     Page(choice);
 
-                    // -----------------------------------------Reads user input-----------------------------------------------------
+                    // -----------------------------------------Reads user input=====================================================
                     chose = true;
                     do
                     {
@@ -118,7 +110,8 @@ namespace TheFay
                         if (Input.Key == ConsoleKey.A)
                         {
                             // Changes page
-                            choice = choice + 1;
+
+                            choice = int.Parse(page[3]);
                             chose = false;
                         }
 
@@ -141,6 +134,35 @@ namespace TheFay
         static void Test()
         {
            
+        }
+
+        static void Title()
+        {
+            //=================================================Title=============================================================
+            Page(choice); // Title
+
+            chose = true;
+            do
+            {
+                ConsoleKeyInfo Input = Console.ReadKey(true);
+
+                if (Input.Key == ConsoleKey.Escape)
+                {
+                    // Changes page
+                    gameLoop = false;
+                    break;
+                }
+
+                else 
+                {
+                    // Changes page
+                    choice = choice + 1;
+                    chose = false;
+                    isStoryActive = true;                    
+                }
+            }
+            while (chose == true);
+            //===================================================================================================================
         }
     }
 }
