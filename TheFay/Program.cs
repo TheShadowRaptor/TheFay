aee439556315bdd999478b5abb3141e362924657
TheShadowRaptor <90637945+TheShadowRaptor@users.noreply.github.com>
2021-11-24 22:59:54 -0400

    Created a split

--- TheFay/Program.cs
+++ TheFay/Program.cs
@@ -8,15 +8,43 @@ namespace TheFay
 {
     class Program
     {
-        static string page;
-        static string[] Story = new string[10];
+        //Determins how much pages the story has
+        static string[] story = new string[10];
 
-        static string[] split = Story.split(';');
-    }
+        //Determins splits value
+        static char[] splits = {';'};
 
-    static void GameLoop()
-    {
-        //Player Input
+        //Determins which array in story you are
+        static int pageNumber = 0;
 
+        //Determins the current page of story array
+        static string[] page = new string[10];
+
+        static void Main(string[] args)
+        {
+            Story();
+            Console.ReadKey(true);
+        }
+        static void Story()
+        {
+            story[0] = "TextOne;SubOne;";
+            story[1] = "Text2";
+            story[2] = "Text3";
+            story[3] = "Text4";
+            story[4] = "Text5";
+            story[5] = "Text6";
+
+            string[] page = story[pageNumber].Split(splits);
+
+            foreach (string sub in page)
+            {
+                Console.WriteLine($"{sub}");
+            }
+        }
+        static void GameLoop()
+        {
+            //Player Input
+
+        }
     }
 }
